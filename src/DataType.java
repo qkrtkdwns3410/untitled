1f6d72bf935aa30bd3f92e92e094872a7b41bdc4
psj <qkrtkdwns3410@naver.com>
2021-06-27 13:27:20 +0900

    6-27

---
+++ src/DataType.java
@@ -0,0 +1,6 @@
+public class DataType {
+    public static void main(String[] args) {
+        System.out.println("Hello World".length());
+        System.out.println("[[name]]... bye".replace("[[name]]","박상준"));
+    }
+}
