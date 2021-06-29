18abedbf875e0cc62a30fdc25b5964c3ca5aca66
psj <qkrtkdwns3410@naver.com>
2021-06-29 18:26:35 +0900

    6-27

---
+++ src/dynamic_beat_8/Main.java
@@ -0,0 +1,10 @@
+package dynamic_beat_8;
+
+public class Main {
+    public final static int SCREEN_WIDTH = 1280;
+    public final static int SCREEN_HEGHT = 720;
+    
+    public static void main(String[] args) {
+        new DynamicBeat();
+    }
+}
